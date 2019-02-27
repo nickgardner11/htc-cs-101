0847eb8e6adaa3612a5924b43444eba1d7c64eb0
nickgardner11 <47343255+nickgardner11@users.noreply.github.com>
2019-02-26 19:27:37 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Week_4___Followalong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           bool snowOnGround = false;
+
+           bool feelingSick = true;
+
+           if (feelingSick == true && foundGloves = true){
+
+               Console.WriteLine("I am gonna build a snowman");
+           }
+               else 
+{
+                Console.WriteLine("Im staying inside bc I dont want H Y P O T E R M I A");
+           }
+
+        }
+    }
+}
